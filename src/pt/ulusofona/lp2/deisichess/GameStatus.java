f77422c9c92cea71afa2258a1d8f20abd05fc5f0
Bruno Ramos <goobrunoblue@gmail.com>
2023-11-29 15:25:20 +0000

    commit 7.5 - part 2

--- src/pt/ulusofona/lp2/deisichess/GameStatus.java
+++ src/pt/ulusofona/lp2/deisichess/GameStatus.java
@@ -163,12 +163,13 @@ public class GameStatus {
         }
         historicRoundDetails.add(currentTeam+"|"+roundCounter +"|"+ consecutivePlays +"@"+teamStatistics[0]+"@"+teamStatistics[1]+"@"+stringBuilder);
 
-        ArrayList<String[]> cloneBoard = new ArrayList<>(theBoard.getBoardMap());
+        ArrayList<String[]> boardMap = theBoard.getBoardMap();
+        int size = boardMap.size();
         StringBuilder sb = new StringBuilder();
-        for (int i=0; i< cloneBoard.size(); i++){
-            String resultado = String.join(":", cloneBoard.get(i));
+        for (int i=0; i< size; i++){
+            String resultado = String.join(":", boardMap.get(i));
             sb.append(resultado);
-            if (i< cloneBoard.size()-1){
+            if (i< size-1){
                 sb.append("-");
             }
 
